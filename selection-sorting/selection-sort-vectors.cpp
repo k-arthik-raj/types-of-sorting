6845a0d87ecd6c865f4c9c4a29dda79c0f4ef34e
k-arthik-raj <91473761+k-arthik-raj@users.noreply.github.com>
2022-04-03 23:45:13 +0530

    Update selection-sort-vectors.cpp

--- selection-sorting/selection-sort-vectors.cpp
+++ selection-sorting/selection-sort-vectors.cpp
@@ -1,1 +1,32 @@
-
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void selection_sort(vector<int> arr)
+{
+    int n = arr.size();
+    for(auto i = arr.begin();i != arr.end()-1; i++)
+    {
+        auto j = arr.at(i)+1,min = arr.at(i);
+        while(j != arr.end())
+        {
+            if(*min>*j)
+            {
+                min = j;
+            }
+        }
+        swap(i,min);
+    }
+}
+void display_arr(vector<int> arr)
+{
+    int n = arr.size();
+    for(auto i = arr.begin();i != arr.end();i++)
+        cout<<*i<<" ";
+}
+int main()
+{
+    vector<int> arr = {100,23,43,12,16};
+    selection_sort(arr);
+    display_arr(arr);
+}
