e6c597b348eab3e8fc265f6cd934b6c0e383dbd8
k-arthik-raj <91473761+k-arthik-raj@users.noreply.github.com>
2022-04-04 20:33:25 +0530

    Update selection-sort-vectors.cpp

--- selection-sorting/selection-sort-vectors.cpp
+++ selection-sorting/selection-sort-vectors.cpp
@@ -2,31 +2,37 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-void selection_sort(vector<int> arr)
-{
-    int n = arr.size();
-    for(auto i = arr.begin();i != arr.end()-1; i++)
-    {
-        auto j = arr.at(i)+1,min = arr.at(i);
-        while(j != arr.end())
-        {
-            if(*min>*j)
-            {
-                min = arr.at(j);
-            }
-        }
-        swap(arr.at(i),arr.at(min));
-    }
-}
 void display_arr(vector<int> arr)
 {
-    int n = arr.size();
     for(auto i = arr.begin();i != arr.end();i++)
         cout<<*i<<" ";
 }
+
+vector<int> selection_sort(vector<int> arr)
+{
+    int n = arr.size();
+    for(auto i = arr.begin();i != arr.end()-2; i++)
+    {
+        auto j = i+1;
+        auto min = i;
+        while(j != arr.end()-1)
+        {
+            
+            if(*min>*j)
+            {
+                min = j;
+            }
+            j++;
+        }
+        iter_swap(min,i);
+      
+    }
+    return arr;
+}
+
 int main()
 {
     vector<int> arr = {100,23,43,12,16};
-    selection_sort(arr);
+    arr = selection_sort(arr);
     display_arr(arr);
 }
